771f49b27ae876c90725c96d003009dac1a50334
Camille Emig <Camille.emig@gmail.com>
2017-03-14 10:59:23 -0400

    adding comments

--- proj07.cpp
+++ proj07.cpp
@@ -1,3 +1,19 @@
+/*************************************
+ * Project 7
+ * Camille Emig
+ * Section 003
+ * 03-14-2017
+ *
+ * Function to clean a string
+ * Function to generate a vector of n_grams
+ * Function to print a map
+ * Function to print a vector
+ * Function to create a map from a line
+ * Functions to compare map elements for sorting
+ * Function to return a sorted vector of a certain length
+ * Function to calculate ochiai
+ **************************************/
+
 #include<string>
 using std::string;
 #include<map>
@@ -16,23 +32,25 @@ using std::cout; using std::skipws; using std::noskipws;
 using std::sort; using std::set_intersection;
 #include <cmath>
 using std::sqrt;
+
+
 string clean_string(string w){
 	stringstream ss;
 	string s;
-	for(auto ch : w){
-		if(isalpha(ch)){
-			ss << (char)tolower(ch);
+	for(auto ch : w){ //Check each character in the string
+		if(isalpha(ch)){ //If the character is alphabetic
+			ss << (char)tolower(ch); //add the lower case of the character
 		}
 	}
-	ss >> s;
+	ss >> s; //output the new string to a variable to return
 	return s;
 }
 
 vector<string> generate_ngrams(string w, size_t n){
 	vector<string> vec;
-	if(w.size() != 0){
-		for(int i = 0; i < (w.size()-n + 1); i ++){
-			vec.push_back(w.substr(i, n));
+	if(w.size() != 0){ //Makes sure there is at least one character
+		for(int i = 0; i < (w.size()-n + 1); i ++){ //w.size()-n + 1 makes it that it stops at the correct char
+			vec.push_back(w.substr(i, n)); //adds the n_gram of length n to the vector
 		}
 	}
 	return vec;
@@ -42,10 +60,10 @@ string map_to_string(map<string,long>  &m){
 	stringstream ss;
 	string s;
 	for(auto elem : m){
-    	ss << elem.first << ":" << elem.second << ", ";
+    	ss << elem.first << ":" << elem.second << ", "; //add each map pair to the string
     }
-	s = ss.str();
-	s = s.substr(0, s.size() - 2);
+	s = ss.str(); //move the new string to a variable
+	s = s.substr(0, s.size() - 2); //Delete the last space and comma
 	return s;
 }
 
@@ -53,18 +71,19 @@ string vector_to_string(vector<pair<string,long>> &v){
 	stringstream ss;
 	string s;
 	for(auto elem : v){
-    	ss << elem.first << ":" << elem.second << ", ";
+    	ss << elem.first << ":" << elem.second << ", ";  //add each vector pair to the string
     }
-	s = ss.str();
-	s = s.substr(0, s.size() - 2);
+	s = ss.str(); //move the string to a new variable
+	s = s.substr(0, s.size() - 2); //Delete the last space and comma
 	return s;
 }
 
 void process_line(map<string, long>& m, string line, size_t n){
-	line = clean_string(line);
-	vector<string> vec = generate_ngrams(line, n);
+	line = clean_string(line); //strips the line
+	vector<string> vec = generate_ngrams(line, n); //Generates a vector of the n_grams
     for(string s : vec){
-        if(m.find(s) != m.end()){
+		//Count how many occurances of each element there are
+        if(m.find(s) != m.end()){ //Use m[s] += 1?
             m.at(s) += 1;
         }
         else{
@@ -74,27 +93,28 @@ void process_line(map<string, long>& m, string line, size_t n){
 }
 
 bool pair_string_lessthan(const pair<string, long> &p1, const pair<string, long> &p2){
-	return p1.first < p2.first;
+	return p1.first < p2.first; //Comparing the strings
 }
 
 bool pair_frequency_greaterthan(const pair<string, long> &p1, const pair<string, long> &p2){
-	if(p1.second != p2.second){
+	if(p1.second != p2.second){ //If the number of items isn't the same
 		return p1.second > p2.second;
 	}
-	else{
+	else{ //Otherwise sort by the string of each
 		return p1.first < p2.first;
 	}
 }
 
 vector<pair<string, long>> top_n(map<string, long> &m, int num){
 	vector<pair<string, long> > v;
-    copy(m.begin(), m.end(), back_inserter(v));
-    sort(v.begin(), v.end(), pair_frequency_greaterthan);
-    vector<pair<string, long>> return_v(v.begin(), v.begin() + num);
-    return return_v;
+    copy(m.begin(), m.end(), back_inserter(v)); //Copy each element in the map to the vector
+    sort(v.begin(), v.end(), pair_frequency_greaterthan); //Sort the vector
+    vector<pair<string, long>> return_v(v.begin(), v.begin() + num); //new vector of top num items
+    return return_v; //return the shortened vector
 }
 
 double ochiai(map<string, long> &m1, map<string, long> &m2){
+	//THIS DOESN'T WORK
 	vector<pair<string, long> > v(m1.size()); 
 	vector<pair<string, long> >::iterator it;
 	top_n(m1, m1.size()); //sort m1
